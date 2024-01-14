04c1b156bc730591ab4f21fb3280288a4fbdcd65
Stephen Garcia <garciastephen5280@gmail.com>
2024-01-14 14:37:24 -0700

    changes to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -15,8 +15,6 @@ CREATE TABLE role (
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL UNSIGNED NOT NULL,
   department_id INT UNSIGNED,
-  INDEX idx_department_id (department_id),
-  CONSTRAINT fk_role_department
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
@@ -29,13 +27,9 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT UNSIGNED,
   manager_id INT UNSIGNED,
-  INDEX idx_role_id (role_id),
-  INDEX idx_manager_id (manager_id),
-  CONSTRAINT fk_employee_role
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE CASCADE,
-  CONSTRAINT fk_employee_manager
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
